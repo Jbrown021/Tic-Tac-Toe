16456d95c27c4c1d7849012d31b99c915848ca2c
Josh Brown <joshuaebrown021@gmail.com>
2022-03-08 04:10:44 -0500

    updating logic

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 // logic of tic tac toe
 const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle'
-// sellect all cells
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
 const cellElements = document.querySelectorAll('[data-cell]')
-
+const board = document.getElementById('board')
+const winningMessageTextElement = document.querySelector('[data-winning-message-text')
 let circleTurn
-// loops
-//event listeners
 
+startGame()
+
+
+function startGame() {
+    // loops
+//event listeners
+circleTurn = false
 cellElements.forEach(cell => {
     cell.addEventListener('click', handleClick,{ once: true})
 })
+setBoardHoverClass()
+}
 
 function handleClick(e) {
     const cell = e.target
@@ -18,11 +35,47 @@ function handleClick(e) {
     // placeMark
     placeMark(cell, currentClass)
     // check for win
+    if (checkWin(currentClass)) {
+        endGame(false)
+    }
     // check for draw
     // switch turns
+    swapTurns()
+    setBoardHoverClass()
 }
+
+function endGame(draw) {
+    if (draw) {
+
+    } else {
+        winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+    }    
+    winningMessageTextElement.classList.add('show') //stoping point
+    }
 
 
 function placeMark(cell, currentClass) {
     cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+    circleTurn = !circleTurn
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS)
+    board.classList.remove(CIRCLE_CLASS)
+    if (circleTurn) {
+        board.classList.add(CIRCLE_CLASS)
+    } else {
+        board.classList.add(X_CLASS)
+    }
+}
+
+function checkWin(currentClass){
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass)
+        })
+    })
 }
